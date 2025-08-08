7a32c6a829bcd29c733b89056b47503e95881ca8
Zeetch39 <zahid.s@zeetch.pro>
2025-08-08 06:37:48 +0500

    Zahid - First Push

---
+++ CICDDemo/CICDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDDemoApp.swift
+//  CICDDemo
+//
+//  Created by Engr. Zahid Shafiq on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CICDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
